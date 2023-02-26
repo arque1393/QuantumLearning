5c8cecba3ef133ee73d4d3695b3c9c14f7a191b7
Aritra Ranjan Chowdhury <aritra1393@gmail.com>
2023-02-26 14:59:51 +0000

    update

---
+++ QuantumInfoWithQiskit/myUtility.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+class Bin(object):
+    def __init__(self,a, format):
+        super().__init__()
+        self.str = 
+        
+def desimal_to_bitArray(a:int):
+    pass
+
+
+a = Bin()
